d5778475d0a60fb0c283644704a70467f8c457e2
Galus1 <79083111+Galus1@users.noreply.github.com>
2022-07-12 15:45:00 +0300

    new2

---
+++ src/com/max/idea/test02.java
@@ -0,0 +1,8 @@
+package com.max.idea;
+
+public class test02 {
+    public static void main(String[] args) {
+        int [] nums ={5,2,3,4,1};
+        System.out.println("сумма среднего и последнего элементов" +"=" +(nums[nums.length-3]+nums[nums.length-1]));
+
+    }}
