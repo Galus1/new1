fe95ec80e2c635c0efbb9430226395052e44b079
Galus1 <79083111+Galus1@users.noreply.github.com>
2022-07-12 13:10:01 +0300

    new

---
+++ src/com/max/idea/test01.java
@@ -0,0 +1,14 @@
+package com.max.idea;
+
+public class test01 {
+        public static void main(String[] args) {
+            double x= (4.9*10*10308);
+            System.out.println(x);
+            byte y=127;
+            System.out.println(y);
+
+        }
+
+
+    }
+
